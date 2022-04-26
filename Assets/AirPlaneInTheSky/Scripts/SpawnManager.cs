181925366804db522e3086b2e2d53fe9ce7e682a
Victor Matheus <victormatheus183@gmail.com>
2022-04-26 09:46:32 -0300

    New spawn logic
    array of obstacles applied

--- Assets/AirPlaneInTheSky/Scripts/SpawnManager.cs
+++ Assets/AirPlaneInTheSky/Scripts/SpawnManager.cs
@@ -14,23 +14,18 @@ public class SpawnManager : MonoBehaviour
     float yObstacle = 600;
     float zObstacle = 7000;
 
-    int index;
-
     public GameObject target;
     public GameObject fuel;
     public GameObject ammo;
     public GameObject[] obstacle = new GameObject[5];
 
-   
-
     // Start is called before the first frame update
     void Start()
     {
-        RandomObstacle();
-        StartCoroutine(GameObjectSpawnTimeHandle(target, -xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 2f));
-        StartCoroutine(GameObjectSpawnTimeHandle(fuel, -xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 15f));
-        StartCoroutine(GameObjectSpawnTimeHandle(ammo, -xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 40f));
-        StartCoroutine(GameObjectSpawnTimeHandle(obstacle[index], -xObstacle, xObstacle, -yObstacle, yObstacle, zObstacle, zObstacle, 17f));
+        StartCoroutine(GameObjectSpawnTimeHandle(-xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 2f, target));
+        StartCoroutine(GameObjectSpawnTimeHandle(-xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 15f, fuel));
+        StartCoroutine(GameObjectSpawnTimeHandle(-xSpawnRange, xSpawnRange, -ySpawnRange, ySpawnRange, zMinSpawnRange, zMaxSpawnRange, 40f, ammo));
+        StartCoroutine(GameObjectSpawnTimeHandle(-xObstacle, xObstacle, -yObstacle, yObstacle, zObstacle, zObstacle, 17f, null, obstacle));
     }
 
     // Update is called once per frame
@@ -39,28 +34,37 @@ public class SpawnManager : MonoBehaviour
 
         
     }
-    void RespawnGameObject(GameObject gameObject, float xMinRange, float xMaxRange, float yMinRange, float yMaxRange, float zMinRange, float zMaxRange)
+   
+    void RespawnGameObject(float xMinRange, float xMaxRange, float yMinRange, float yMaxRange, float zMinRange, float zMaxRange, GameObject gameObject = default, GameObject[] gameObjects = default)
     {
         float xPos = Random.Range(xMinRange, xMaxRange);
         float yPos = Random.Range(yMinRange, yMaxRange);
         float zPos = Random.Range(zMinRange, zMaxRange);
 
         Vector3 respawnPos = new Vector3(xPos, yPos, zPos);
-        Instantiate(gameObject, respawnPos, gameObject.transform.rotation);
+        
+        if (!(gameObjects == null))
+        {
+            int index = RandomObstacle();
+            Instantiate(gameObjects[index], respawnPos, gameObjects[index].transform.rotation);
+        }
+        else
+        {
+            Instantiate(gameObject, respawnPos, gameObject.transform.rotation);
+        }
     }
 
-    IEnumerator GameObjectSpawnTimeHandle(GameObject gameObject, float xMinRange, float xMaxRange, float yMinRange, float yMaxRange, float zMinRange, float zMaxRange, float repeatAfter)
+    IEnumerator GameObjectSpawnTimeHandle(float xMinRange, float xMaxRange, float yMinRange, float yMaxRange, float zMinRange, float zMaxRange, float repeatAfter, GameObject gameObject = default(GameObject), GameObject[] gameObjects = default(GameObject[]))
     {
         while (true)
         {
             yield return new WaitForSeconds(repeatAfter);
-            RandomObstacle();
-            RespawnGameObject(gameObject, xMinRange, xMaxRange, yMinRange, yMaxRange, zMinRange, zMaxRange);
+            RespawnGameObject(xMinRange, xMaxRange, yMinRange, yMaxRange, zMinRange, zMaxRange, gameObject, gameObjects);
         }
     }
 
-    void RandomObstacle()
+    int RandomObstacle()
     {
-        index = Random.Range(0, 5);
+        return Random.Range(0, 5);
     }
 }
